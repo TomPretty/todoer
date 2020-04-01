8d103cf98f6293bf241145ba08c4f6baa0e536f2
TomPretty <tompretty2@gmail.com>
2020-04-01 16:25:04 +0100

    Allow changing the todo directory with the TODOER_TODO_DIR env var

--- todoer.sh
+++ todoer.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env zsh
 
-TODO_DIR="$HOME/todos"
+DEFAULT_TODO_DIR="$HOME/todos"
+
+if [[ -z "${TODOER_TODO_DIR}" ]]; then
+  TODO_DIR="$DEFAULT_TODO_DIR"
+else
+  TODO_DIR="$TODOER_TODO_DIR"
+fi
 
 echo "$TODO_DIR/$(dater $1).md"
